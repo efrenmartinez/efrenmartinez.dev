4ce842d3602a59f3f4b3a986f0437968df322a23
efrenmartinez <developer.emartinez@gmail.com>
2020-08-07 17:41:00 -0500

    :speech_balloon: Add Text in Sectionintro

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <h1>Hola!! Soy Efrén Martínez Rodríguez</h1>
+    <h1>👋 Hola!! Soy Efrén Martínez Rodríguez</h1>
     <h2>
       Desarrollador Frontend | Entusiasta de Python
     </h2>
@@ -12,14 +12,22 @@
       una agencia digital con clientes como Suzuki Mexico, Michelin, AGCO
       México.
     </p>
-    <h3>STACK</h3>
+    <h3>EXPERIENCIA</h3>
+    <p>
+      He tenido la oportunidad de trabajar en
+      <a href="https://www.grupoeducare.com/web/" target="_blank"
+        >Grupo Educare</a
+      >, empresa que se dedica a la realización de libros interactivos, en donde
+      realice proyectos con
+      <span class="nx-bold">PHP, MySQL.</span>
+    </p>
     <p>
       Mi stack de día a día es creando aplicaciones web usando
-      <span class="nx-bold">Quasar</span>, un framework de
+      <a href="https://quasar.dev/" target="_blank">Quasar</a>, un framework de
       <span class="nx-bold">VueJS</span> el cual permite escribir codigo una
       sola vez y poder desplegarlo como una aplicación SPA, SSR, PWA, aplicación
-      movil o como aplicación multiplataforma. Ademas de escribir codigo CSS
-      usando SASS.
+      movil o como aplicación multiplataforma. Ademas de escribir codigo
+      <span class="nx-bold">CSS</span> usando <span class="nx-bold">SASS</span>.
     </p>
     <p>
       Cuando no estoy desarrollando por lo regular me encuentro leyendo, jugando
