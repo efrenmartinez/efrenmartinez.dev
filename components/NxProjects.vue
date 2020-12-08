f4cd1c582d3f53e350209d8c1bd2a1e790249bd0
efrenmartinez <developer.emartinez@gmail.com>
2020-12-07 23:48:43 -0600

    :iphone: Responsive Project

--- components/NxProjects.vue
+++ components/NxProjects.vue
@@ -65,16 +65,16 @@ export default {
       }
     }
   }
-  // @media screen and (max-width: 900px) {
-  //   & {
-  //     height: auto;
-  //     flex-direction: column;
-  //     row-gap: 20px;
-  //     &-items {
-  //       width: 100%;
-  //       height: 100%;
-  //     }
-  //   }
-  // }
+  @media screen and (max-width: 900px) {
+    & {
+      height: auto;
+      flex-direction: column;
+      row-gap: 20px;
+      &-items {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
 }
 </style>
